39e3b3190cc13bc5a048c1067e8a39fd2e0903f4
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-07-24 22:58:38 +0600

    added typescript basic

---
+++ output/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// let playerName ="shahadat";
+// let age =35;
+// console.log(playerName)
+// age="shakib"
+// console.log(playerName)
+// let playerName;
+// console.log(playerName)
+// playerName ="masrafi"
+// playerName =34
+// console.log(playerName)
---
+++ src/script.ts
@@ -0,0 +1,18 @@
+// let playerName ="shahadat";
+// let age =35;
+
+// console.log(playerName)
+
+// age="shakib"
+
+// console.log(playerName)
+
+// let playerName;
+
+// console.log(playerName)
+// playerName ="masrafi"
+// playerName =34
+// console.log(playerName)
+
+
+
